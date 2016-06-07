56fcc00b6b89625c2972ce84f17e5f159cc7d41b
je-cg <jecg03@hotmail.com>
2016-06-06 20:40:34 -0700

    implented request for todo list

--- public/scripts/todoCtrl.js
+++ public/scripts/todoCtrl.js
@@ -1,8 +1,15 @@
 (function (){
-	angular.module("todoApp").controller("todoCtrl", ["$scope", "$http", todoController]);
+	angular.module("TodoApp").controller("todoCtrl", ["$scope", "$http", todoController]);
 
 	function todoController($scope, $http){
-		
+		$scope.list= [];
+		$http.get("/resources/list.json").then(function(response){
+			console.log("succes ",response.data);
+		}, 
+		function(response){
+			console.log(response);
+		});
 	}
 
-})()
+
+})();
